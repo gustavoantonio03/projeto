e7d1a800032778f8282a008ba2d3b624b1ddff37
Gustavo Antônio <gustavorubronegroantonio@gmail.com>
2026-04-01 01:11:38 -0300

    segunda alteração

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
-    function toggleModo (){
-        document.body.classList.toggle("dark-mode");
+function toggleModo() {
+    document.body.classList.toggle("dark-mode");
+
+    const btn = document.querySelector(".btn-modo");
+
+    if (document.body.classList.contains("dark-mode")) {
+        btn.textContent = "Modo claro ☀️";
+    } else {
+        btn.textContent = "Modo escuro 🌙";
     }
+}
+
+function toggleConteudo(id) {
+    const elemento = document.getElementById(id);
+
+    if (elemento.style.display === "block") {
+        elemento.style.display = "none";
+    } else {
+        elemento.style.display = "block";
+    }
+}
