e7a1be2e4c6e2f8371cdbaf82e07a279b4cac839
Gustavo Antônio <gustavorubronegroantonio@gmail.com>
2026-04-06 18:11:47 -0300

    commit para adição do contato

--- js/script.js
+++ js/script.js
@@ -24,3 +24,36 @@ function mostrarAnoAtual(){
     const anoAtual = new Date().getFullYear();
     document.getElementById("ano-atual").textContent = anoAtual;
 }
+
+function enviarMensagem(event) {
+    event.preventDefault();
+
+    const nome = document.getElementById("nome").value;
+    const email = document.getElementById("email").value;
+    const assunto = document.getElementById("assunto").value; 
+    const mensagem = document.getElementById("mensagem").value;
+
+    if (!nome || !email) {
+        alert("Por favor, preencha os campos de nome e email.");
+        return;
+    }
+
+    if (!email.includes("@")) {
+        alert("Por favor, insira um email válido.");
+        return;
+    }
+
+    if (!assunto) {
+        alert("Por favor, informe o assunto.");
+        return;
+    }
+
+    if (!mensagem) {
+        alert("Por favor, escreva uma mensagem.");
+        return;
+    }
+
+    alert("Mensagem enviada com sucesso! Obrigado por entrar em contato.");
+
+    document.getElementById("form-contato").reset();
+}
